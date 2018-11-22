d4b90e223aa8d0270238cfa495d1b5e1304557e1
takohaka <takohaka@gmail.com>
2018-11-22 18:19:40 +0900

    chat追加

---
+++ pages/chat.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="playerElement" style="width:100%; height:0; padding:0 0 56.25% 0"></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: "//player.wowza.com/player/latest/wowzaplayer.min.js"}
+      ]
+    }
+  },
+  mounted() {
+    WowzaPlayer.create('playerElement',
+      {
+        "license":"PLAY2-7rJuG-bmemr-Buajc-6TNau-zyhZC",
+        "title":"試験放送！！",
+        "description":"",
+        "sourceURL":"http%3A%2F%2F54.249.97.127%3A1935%2Flive%2FmyStream%2Fplaylist.m3u8",
+        "autoPlay":false,
+        "volume":"75",
+        "mute":false,
+        "loop":false,
+        "audioOnly":false,
+        "uiShowQuickRewind":true,
+        "uiQuickRewindSeconds":"30"
+      }
+    );
+  }
+}
+</script>
